8b5a247abc920a67b70ef50bd2fc8805a42efef4
sabertazimi <sabertazimi@gmail.com>
2018-07-07 16:26:23 +0800

    add(v1.0.0): add test demo

---
+++ index.js
@@ -0,0 +1,42 @@
+var moment = require('moment');
+
+module.exports = {
+  book: {
+    assets: './assets',
+    css: [
+      'footer.css'
+    ],
+  },
+  hooks: {
+    'page:before': function (page) {
+      var _label = 'File Modify: ';
+      var _format = 'YYYY-MM-DD HH:mm';
+      var _copy = 'powered by Gitbook';
+
+      if (this.options.pluginsConfig['timefooter']) {
+        _label = this.options.pluginsConfig['timefooter']['modify_label'] || _label;
+        _format = this.options.pluginsConfig['timefooter']['modify_format'] || _format;
+
+        var _c = this.options.pluginsConfig['timefooter']['copyright'];
+        _copy = _c ? _c + ' all right reserved，' + _copy : _copy;
+      }
+
+      console.log(page);
+
+      var _copy = '<span class="copyright">' + _copy + '</span>';
+      var str = ' \n\n<footer class="page-footer">' + _copy +
+        '<span class="footer-modification">' +
+        _label +
+        '\n{{file.mtime | date("' + _format +
+        '")}}\n</span></footer>';
+
+      page.content = page.content + str;
+      return page;
+    }
+  },
+  filters: {
+    date: function (d, format) {
+      return moment(d).format(format);
+    }
+  }
+};
