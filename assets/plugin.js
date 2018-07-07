752482efbd7d302ac4899dab42005affb5ec278c
sabertazimi <sabertazimi@gmail.com>
2018-07-07 19:51:57 +0800

    2.2.0

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,6 +1,6 @@
 var path = require('path');
 
-var Git = require('nodegit');
+// var Git = require('nodegit');
 var moment = require('moment');
 
 module.exports = function (book, page) {
@@ -20,34 +20,35 @@ module.exports = function (book, page) {
         var historyFile = book.config.get('root', '') + page.path;
         var repo;
         console.log(historyFile);
+        return page;
 
-        return Git.Repository.open(path.resolve('./.git'))
-            .then(function (r) {
-                repo = r;
-                return repo.getMasterCommit();
-            })
-            .then(function (firstCommitOnMaster) {
-                var walker = repo.createRevWalk();
-                walker.push(firstCommitOnMaster.sha());
-                walker.sorting(Git.Revwalk.SORT.Time);
+        // return Git.Repository.open(path.resolve('./.git'))
+        //     .then(function (r) {
+        //         repo = r;
+        //         return repo.getMasterCommit();
+        //     })
+        //     .then(function (firstCommitOnMaster) {
+        //         var walker = repo.createRevWalk();
+        //         walker.push(firstCommitOnMaster.sha());
+        //         walker.sorting(Git.Revwalk.SORT.Time);
 
-                return walker.fileHistoryWalk(historyFile, 2);
-            })
-            .then((resultingArrayOfCommits) => {
-                moment.locale();
-                var dateStr = moment().format(_format);
+        //         return walker.fileHistoryWalk(historyFile, 2);
+        //     })
+        //     .then((resultingArrayOfCommits) => {
+        //         moment.locale();
+        //         var dateStr = moment().format(_format);
 
-                if (resultingArrayOfCommits.length > 0) {
-                    var commit = resultingArrayOfCommits[0].commit;
-                    var date = commit.date();
-                    dateStr = moment(date).format(_format);
-                }
+        //         if (resultingArrayOfCommits.length > 0) {
+        //             var commit = resultingArrayOfCommits[0].commit;
+        //             var date = commit.date();
+        //             dateStr = moment(date).format(_format);
+        //         }
 
-                _copy = '<span class="copyright">' + _copy + '</span>';
-                var str = ' \n\n<footer class="page-footer">' + _copy +
-                    '<span class="footer-modification">' + _label +
-                    '\n' + dateStr + '\n</span></footer>';
-                page.content = page.content + str;
-                return page;
-            });
+        //         _copy = '<span class="copyright">' + _copy + '</span>';
+        //         var str = ' \n\n<footer class="page-footer">' + _copy +
+        //             '<span class="footer-modification">' + _label +
+        //             '\n' + dateStr + '\n</span></footer>';
+        //         page.content = page.content + str;
+        //         return page;
+        //     });
 }
